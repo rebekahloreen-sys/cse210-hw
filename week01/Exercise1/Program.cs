4e00f05d071145c2d0bc2397ee8d8df999d70c62
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-09-04 02:20:23 -0600

    Changed Text

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
