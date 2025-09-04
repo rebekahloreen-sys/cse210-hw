8354aae6669b37543e0290f49d26f8d6d56e41f0
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-09-04 02:47:48 -0600

    Added Grading code

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string stringPercentage = Console.ReadLine();
+
+        int percentage = int.Parse(stringPercentage);
+        string letter;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congradulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed this time, but you can always try again!");
+        }
     }
-}
+} 
