6c5d9de7afcbeaa4348d5cb186f4b578e9b66317
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-09-05 18:51:03 -0600

    Lists

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+        List<int> numbersInList = new List<int>();
+        int number = 1000;
+        int addedNumbers = 0;
+        int largestNumber = -1000000000;
+
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+
+            Console.Write("Enter a number: ");
+
+            string entry = Console.ReadLine();
+            number = int.Parse(entry);
+            if (number != 0)
+            {
+                numbersInList.Add(number);
+            }
+
+        } while (number != 0);
+
+
+
+        foreach (int place in numbersInList)
+        {
+            addedNumbers += place;
+            if (place > largestNumber)
+            {
+                largestNumber = place;
+            }
+        }
+
+        double average = (double)addedNumbers / numbersInList.Count;
+
+        Console.WriteLine($"The sum is: {addedNumbers}");
+
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is {largestNumber}");
+
+
     }
 }
