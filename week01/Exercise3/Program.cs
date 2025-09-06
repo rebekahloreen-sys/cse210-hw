bad9dede60c01653b59bc7d0bda040b336c18661
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-09-05 18:15:38 -0600

    Number Guessing Game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
+
+        int guessNum = 0;
+  
+
+        while (magicNum != guessNum)
+        {
+
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+
+            guessNum = int.Parse(guessNumber);
+
+
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            if (guessNum == magicNum)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
