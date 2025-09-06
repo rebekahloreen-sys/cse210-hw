2ad5b1d5c72eb656ba3dcc18e595fdde365e256e
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-09-05 19:19:43 -0600

    Functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+        static int GetFavoriteNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string entry = Console.ReadLine();
+
+            return int.Parse(entry);
+        }
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"{name}, the square of your number is {number}");
+        }
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = GetFavoriteNumber();
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(name, squaredNumber);
+
+
     }
 }
