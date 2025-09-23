b8e0edeef043afb28c4f0ebe8efeb108c3202a88
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-09-23 02:45:33 -0600

    Fractions!!

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+
+        Fraction dec = new Fraction(1, 3);
+        Console.WriteLine(dec.GetDecimalValue());
+
     }
+
 }
+
+    
