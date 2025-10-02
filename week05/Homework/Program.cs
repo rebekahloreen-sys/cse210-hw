51ebf4a685b89cbbb5f2370df8558ed9b4d16790
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-10-02 02:34:58 -0600

    Complete assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assign1 = new Assignment("Sandra", "Nature Studies");
+        MathAssignment mathAssign = new MathAssignment("Sandra", "Nature Studies", "7.3", "8-19");
+        Console.WriteLine(assign1.GetSummary());
+        Console.WriteLine(mathAssign.GetHomeworkList());
+
+
+
+        Assignment assign2 = new Assignment("Sandra", "Medieval History");
+        WritingAssignment writeAssign = new WritingAssignment("Sandra", "Medieval History", "Why Certain Messed-up Things Happened");
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(writeAssign.GetWritingInformation());
+
     }
 }
