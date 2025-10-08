15400ed67163cecef86058ed6b896135fdda5b47
rebekahloreen-sys <rebekahloreen@gmail.com>
2025-10-08 04:42:30 -0600

    shapes are done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square squa = new Square("Red", 4);
+        shapes.Add(squa);
+
+        Rectangle rect = new Rectangle("Yellow", 9, 3);
+        shapes.Add(rect);
+
+        Circle circ = new Circle("Blue", 6);
+        shapes.Add(circ);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
